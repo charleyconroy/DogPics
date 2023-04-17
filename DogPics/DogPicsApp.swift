749fd32d6bc64a59ac7d71fdb92863cc9bfe674f
Charles Conroy <conroycd@bc.edu>
2023-04-17 19:42:42 -0400

    Initial Commit

---
+++ DogPics/DogPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPicsApp.swift
+//  DogPics
+//
+//  Created by Charley Conroy on 4/17/23.
+//
+
+import SwiftUI
+
+@main
+struct DogPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
